99e5a1e529b7364fce697fe661adf120ab99af5a
FVazquezRisso <facundovazquez321@gmail.com>
2023-12-30 23:28:06 -0300

    Se creó la página following

--- src/app/web/home/following/page.tsx
+++ src/app/web/home/following/page.tsx
@@ -1,3 +1,70 @@
-export default function Latest() {
-  return <h1>Following Page</h1>;
+"use client";
+import PostCard from "../../../ui/PostCard";
+import { useState, useEffect } from "react";
+import { api } from "../../../../services/apiConfig";
+import { PostInterface } from "@/types/types";
+import { useInView } from "react-intersection-observer";
+import { oswald } from "@/app/ui/fonts";
+
+export default function Following() {
+  const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [hasMorePosts, setHasMorePosts] = useState(true);
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
+
+  const getPosts = async () => {
+    try {
+      if (!hasMorePosts) {
+        return;
+      }
+      const username = localStorage.getItem("username");
+      const response = await api.get(
+        `/posts?page=${currentPage}&username=${username}&following=${username}`
+      );
+      if (response.status === 200) {
+        setPosts([...posts, ...response.data.posts]);
+        if (response.data.posts.length < 10) {
+          setHasMorePosts(false);
+          return;
+        }
+        setCurrentPage(currentPage + 1);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, [inView]);
+
+  return (
+    <div className="pt-16 pb-16">
+      {posts.length !== 0 &&
+        posts.map((post: PostInterface) => {
+          return (
+            <PostCard
+              key={post.id}
+              id={post.id}
+              author={post.author}
+              content={post.content}
+              likeCount={post.likeCount}
+              userLikedPost={post.userLikedPost}
+              commentCount={post.commentCount}
+            />
+          );
+        })}
+      <div ref={ref}></div>
+      {!hasMorePosts && (
+        <div className="h-48 flex items-center justify-center">
+          <p className={`${oswald.className} text-3xl text-center`}>
+            <span className="text-primary-200">¡Fin del feed de seguidos!</span> <br /> ¡A
+            esperar más sorpresas!{" "}
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
