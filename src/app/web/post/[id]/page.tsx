499543dfa16e8f5c17961ba78fe49c14e0772b65
FVazquezRisso <facundovazquez321@gmail.com>
2024-01-01 23:15:46 -0300

    Se agregó la fecha detallada en el PostDetail

--- src/app/web/post/[id]/page.tsx
+++ src/app/web/post/[id]/page.tsx
@@ -1,22 +1,27 @@
 "use client";
 import { useState, useEffect } from "react";
 import { api } from "../../../../services/apiConfig";
-import { IoHeartOutline, IoHeartSharp } from "react-icons/io5";
+import {
+  IoHeartOutline,
+  IoHeartSharp,
+  IoEllipsisVerticalSharp,
+} from "react-icons/io5";
 import { VscComment } from "react-icons/vsc";
-import { useRouter } from "next/navigation";
 import CommentCard from "@/app/ui/CommentCard";
 import { oswald } from "@/app/ui/fonts";
 import TextareaAutosize from "react-textarea-autosize";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { CommentInterface, PostInterface } from "../../../../types/types.ts";
+import { CommentInterface, PostInterface } from "../../../../types/types";
 import UserCard from "../../../ui/UserCard";
-import Header from '../../../ui/Header'
-import LoadingScreen from '../../../ui/LoadingScreen'
+import Header from "../../../ui/Header";
+import LoadingScreen from "../../../ui/LoadingScreen";
+import { formatDate } from "../../../../utils/convertDate";
+import { useRouter } from 'next/navigation'
 
 export default function PostDetail({ params }) {
   const { id } = params;
-  const router = useRouter();
+  const router = useRouter()
   const username = localStorage.getItem("username");
   const [post, setPost] = useState<PostInterface | null>(null);
   const avatar = localStorage.getItem("avatar");
@@ -26,7 +31,9 @@ export default function PostDetail({ params }) {
   const [disabledButton, setDisabledButton] = useState(true);
   const [content, setContent] = useState("");
   const [commentCount, setCommentCount] = useState(0);
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const token = localStorage.getItem("token");
 
   const getPost = async () => {
     try {
@@ -40,7 +47,7 @@ export default function PostDetail({ params }) {
     } catch (error) {
       console.error(error);
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
     }
   };
 
@@ -57,7 +64,6 @@ export default function PostDetail({ params }) {
 
   const handleClickHeart = async () => {
     try {
-      const token = localStorage.getItem("token");
       const response = await api.post(`posts/like/${id}`, null, {
         headers: {
           "x-access-token": token,
@@ -79,13 +85,15 @@ export default function PostDetail({ params }) {
   const handleChange = (event) => {
     const { value } = event.target;
     setContent(value);
-    setDisabledButton(value.replace(/\s+/g, ' ').length < 5 || value.replace(/\s+/g, ' ').length > 200);
+    setDisabledButton(
+      value.replace(/\s+/g, " ").length < 5 ||
+        value.replace(/\s+/g, " ").length > 200
+    );
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const token = localStorage.getItem("token");
       const response = await api.post(
         `/comments/${id}`,
         { content: content.replace(/\s+/g, " ") },
@@ -101,6 +109,36 @@ export default function PostDetail({ params }) {
         getComments();
         setContent("");
         setCommentCount(commentCount + 1);
+        setDisabledButton(true);
+      }
+    } catch (error) {
+      toast.error("Error inesperado. Inténtalo de nuevo más tarde.", {
+        position: toast.POSITION.BOTTOM_LEFT,
+        autoClose: 2000,
+      });
+      console.error(error);
+    }
+  };
+
+  const handleShowMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleClickDeletePost = async () => {
+    try {
+      const response = await api.delete(`/posts/${id}`, {
+        headers: {
+          "x-access-token": token,
+        },
+      });
+      if (response.status === 204) {
+        toast.success("Publicación eliminada con éxito", {
+          position: toast.POSITION.BOTTOM_LEFT,
+          autoClose: 1500,
+        });
+        setTimeout(() => {
+          router.back()
+        }, 1500)
       }
     } catch (error) {
       toast.error("Error inesperado. Inténtalo de nuevo más tarde.", {
@@ -117,24 +155,32 @@ export default function PostDetail({ params }) {
   }, []);
 
   if (isLoading) {
-    return <LoadingScreen />
+    return <LoadingScreen />;
   }
 
   return (
     <>
       <div className="mb-16">
-        <Header title={`Publicación de ${post?.author?.username}`}/>
+        <Header title={`Publicación de ${post?.author?.username}`} />
         <article className="w-screen flex flex-col p-6 pb-0 border-b-[1px] border-bg-300">
-          <div className="flex items-center gap-4">
-            <UserCard
-              username={post?.author?.username}
-              avatar={post?.author?.avatar}
-            />
-            <h3 className="text-lg font-bold">{post?.author?.username}</h3>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-4">
+              <UserCard
+                username={post?.author?.username}
+                avatar={post?.author?.avatar}
+              />
+              <h3 className="text-lg font-bold">{post?.author?.username}</h3>
+            </div>
+            {post?.author?.username === username && (
+              <IoEllipsisVerticalSharp size={20} onClick={handleShowMenu} />
+            )}
           </div>
           <p className="overflow-x-hidden whitespace-pre-wrap break-all mt-4">
             {post?.content}
           </p>
+          <span className="text-text-300">
+            {formatDate(post?.createdAt, false)}
+          </span>
           <div className="h-12 flex items-center justify-around py-4">
             <span className="flex items-center gap-1">
               {isLiked ? (
@@ -172,18 +218,21 @@ export default function PostDetail({ params }) {
                   / 200
                 </h4>
               </div>
-              <button
-                className={
-                  disabledButton ? "disabled-button" : "button"
-                }
-              >
+              <button className={disabledButton ? "disabled-button" : "button"}>
                 Comentar
               </button>
             </div>
           </form>
           {comments.length !== 0 &&
-            comments.map(({ id, content, author }) => {
-              return <CommentCard key={id} content={content} author={author} />;
+            comments.map(({ id, content, author, createdAt }) => {
+              return (
+                <CommentCard
+                  key={id}
+                  content={content}
+                  author={author}
+                  createdAt={createdAt}
+                />
+              );
             })}
           {comments.length === 0 && (
             <div className="h-48 flex items-center justify-center">
@@ -195,6 +244,17 @@ export default function PostDetail({ params }) {
             </div>
           )}
         </div>
+        {isMenuOpen && (
+          <div className="no-scroll-container fixed top-0 z-10 bg-black bg-opacity-80">
+            <div className="flex flex-col gap-4 bg-primary-100 items-center py-8 px-12 text-xl rounded-md">
+              <span>Editar</span>
+              <span onClick={handleClickDeletePost}>Eliminar</span>
+              <span onClick={handleShowMenu} className="font-bold">
+                Cerrar
+              </span>
+            </div>
+          </div>
+        )}
       </div>
       <ToastContainer />
     </>
