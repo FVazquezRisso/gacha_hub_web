b847b7f706bb00608171eda55e5cd50dd62b27d0
FVazquezRisso <facundovazquez321@gmail.com>
2024-01-08 09:26:55 -0300

    Se agregó la URL de deploy back

--- src/services/apiConfig.ts
+++ src/services/apiConfig.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  // baseURL: "http://localhost:3001/api",
+  baseURL: "gacha-hub-api.onrender.com/api",
 });
