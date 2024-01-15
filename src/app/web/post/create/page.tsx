f0d9cf4a452e65b05a865428373e434e53857201
FVazquezRisso <facundovazquez321@gmail.com>
2024-01-15 10:06:48 -0300

    Se agregó la opción de publicar en un grupo

--- src/app/web/post/create/page.tsx
+++ src/app/web/post/create/page.tsx
@@ -1,32 +1,36 @@
 "use client";
-import { useState, FormEvent } from "react";
+import { useState, useEffect, FormEvent } from "react";
 import { api } from "@/services/apiConfig";
 import { useRouter } from "next/navigation";
 import { notification } from "@/utils/notification";
 import TextEditor from "@/app/ui/TextEditor";
 import Header from "@/app/ui/Header";
-import cookies from 'js-cookie'
+import cookies from "js-cookie";
+import LoadingScreen from "@/app/ui/LoadingScreen";
 
 export default function PostCreate() {
   const router = useRouter();
   const avatar: any = cookies.get("avatar");
   const username: any = cookies.get("username");
   const [content, setContent] = useState("");
+  const [groupName, setGroupName] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const token = cookies.get("token");
+  const url = window.location.href;
+  const searchParams = new URLSearchParams(new URL(url).search);
+  const groupId = searchParams.get("groupId");
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     try {
-      const token = cookies.get("token");
-      const response = await api.post(
-        "/posts",
-        { content: content },
-        { headers: { "x-access-token": token } }
-      );
+      const body = { content };
+      if (groupId) body.groupId = groupId;
+      const response = await api.post("/posts", body, {
+        headers: { "x-access-token": token },
+      });
       if (response.status === 201) {
         notification("success", "Publicación creada con éxito.");
-        setTimeout(() => {
-          router.back();
-        }, 1500);
+        router.back();
       }
     } catch (error) {
       notification("error", "Error inesperado. Inténtalo de nuevo más tarde.");
@@ -34,10 +38,40 @@ export default function PostCreate() {
     }
   };
 
+  const getGroup = async () => {
+    if (!groupId) return;
+    try {
+      const response = await api.get(`/groups/${groupId}`, {
+        headers: {
+          "x-access-token": token,
+        },
+      });
+      if (response.status === 200) {
+        setGroupName(response.data.name);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (groupId) {
+      getGroup();
+    } else {
+      setIsLoading(false);
+    }
+  }, [groupId]);
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
+
   return (
     <div className="mb-24">
       <Header title="Crear publicación" />
-      <form onSubmit={handleSubmit} className='px-4'>
+      <form onSubmit={handleSubmit} className="px-4">
         <div className="h-16 w-full flex justify-start items-center gap-4 mt-4">
           <img
             src={avatar}
@@ -46,6 +80,11 @@ export default function PostCreate() {
           />
           <p className="font-medium text-xl">{username}</p>
         </div>
+        {groupName && (
+          <h3 className="text-text-300">
+            Se publicará en el grupo {groupName}
+          </h3>
+        )}
         <TextEditor
           content={content}
           setContent={setContent}
