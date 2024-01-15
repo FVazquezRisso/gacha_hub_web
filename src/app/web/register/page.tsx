c6890ebed17c3aa3f892d5d67150be686cb2d4bc
FVazquezRisso <facundovazquez321@gmail.com>
2024-01-15 10:07:23 -0300

    Se creó el search group

--- src/app/web/register/page.tsx
+++ src/app/web/register/page.tsx
@@ -6,7 +6,7 @@ import { validateFields } from "@/utils/validateField";
 import { api } from "@/services/apiConfig";
 import { useRouter } from "next/navigation";
 import { ChangeEvent, FormEvent } from "react";
-import { notification } from '@/utils/notification'
+import { notification } from "@/utils/notification";
 
 export default function Register() {
   const router = useRouter();
@@ -33,9 +33,7 @@ export default function Register() {
       const response = await api.post("/users/register", data);
       if (response.status === 201) {
         notification("success", "Cuenta creada con éxito.");
-        setTimeout(() => {
-          router.push("/web/login");
-        }, 1500);
+        router.push("/web/login");
       }
     } catch (error) {
       notification("error", "Error inesperado. Inténtalo de nuevo más tarde.");
@@ -52,66 +50,66 @@ export default function Register() {
   }, [data]);
 
   return (
-      <form onSubmit={handleSubmit} className="no-scroll-container gap-4">
-        <div className="flex items-center">
-          <Link href="/">
-            <IoMdArrowRoundBack size={32} />
-          </Link>
-          <h1 className="text-5xl text-text-200 text-center m-4">Registro</h1>
-        </div>
-        <label>
-          <input
-            type="text"
-            name="username"
-            placeholder="Nombre de usuario"
-            autoComplete="off"
-            className="form-input-text"
-            onChange={handleChange}
-            onPaste={(e) => e.preventDefault()}
-          />
-          {errors.username && (
-            <p className="text-error-100 text-left mt-2">{errors.username}</p>
-          )}
-        </label>
-        <label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Contraseña"
-            autoComplete="off"
-            className="form-input-text"
-            onChange={handleChange}
-            onPaste={(e) => e.preventDefault()}
-          />
-          {errors.password && (
-            <p className="text-error-100 text-left mt-2">{errors.password}</p>
-          )}
-        </label>
-        <label>
-          <input
-            type="password"
-            name="confirmPassword"
-            autoComplete="off"
-            placeholder="Repetir contraseña"
-            className="form-input-text"
-            onChange={handleChange}
-            onPaste={(e) => e.preventDefault()}
-          />
-          {errors.confirmPassword && (
-            <p className="text-error-100 text-left mt-2">
-              {errors.confirmPassword}
-            </p>
-          )}
-        </label>
-        <button
-          className={disabledButton ? "disabled-button mt-4" : "button mt-4"}
-          disabled={disabledButton}
-        >
-          Crear cuenta
-        </button>
-        <Link href="/web/login" className="link mt-4">
-          Ya tengo una cuenta
+    <form onSubmit={handleSubmit} className="no-scroll-container gap-4">
+      <div className="flex items-center">
+        <Link href="/">
+          <IoMdArrowRoundBack size={32} />
         </Link>
-      </form>
+        <h1 className="text-5xl text-text-200 text-center m-4">Registro</h1>
+      </div>
+      <label>
+        <input
+          type="text"
+          name="username"
+          placeholder="Nombre de usuario"
+          autoComplete="off"
+          className="form-input-text"
+          onChange={handleChange}
+          onPaste={(e) => e.preventDefault()}
+        />
+        {errors.username && (
+          <p className="text-error-100 text-left mt-2">{errors.username}</p>
+        )}
+      </label>
+      <label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Contraseña"
+          autoComplete="off"
+          className="form-input-text"
+          onChange={handleChange}
+          onPaste={(e) => e.preventDefault()}
+        />
+        {errors.password && (
+          <p className="text-error-100 text-left mt-2">{errors.password}</p>
+        )}
+      </label>
+      <label>
+        <input
+          type="password"
+          name="confirmPassword"
+          autoComplete="off"
+          placeholder="Repetir contraseña"
+          className="form-input-text"
+          onChange={handleChange}
+          onPaste={(e) => e.preventDefault()}
+        />
+        {errors.confirmPassword && (
+          <p className="text-error-100 text-left mt-2">
+            {errors.confirmPassword}
+          </p>
+        )}
+      </label>
+      <button
+        className={disabledButton ? "disabled-button mt-4" : "button mt-4"}
+        disabled={disabledButton}
+      >
+        Crear cuenta
+      </button>
+      <Link href="/web/login" className="link mt-4">
+        Ya tengo una cuenta
+      </Link>
+    </form>
   );
 }
